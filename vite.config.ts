95145228e7129ebb55cdcc20312c2e7b738ffd76
Edier Aristizabal <edieraristizabal@gmail.com>
2026-06-01 09:40:17 -0500

    ci: add deployment workflow and configure vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Polla_GEOHAZARD/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
